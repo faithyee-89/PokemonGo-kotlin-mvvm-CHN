9b479a9db50b9999495e0719b953b328f39dd041
faith_yee <faith_yee@163.com>
2023-07-02 21:48:52 +0800

    update libs

--- buildSrc/src/main/java/com/hi/dhl/plugin/BuildConfig.kt
+++ buildSrc/src/main/java/com/hi/dhl/plugin/BuildConfig.kt
@@ -8,10 +8,10 @@ package com.hi.dhl.plugin
  * </pre>
  */
 object BuildConfig {
-    const val compileSdkVersion = 29
+    const val compileSdkVersion = 32
     const val buildToolsVersion = "29.0.3"
     const val minSdkVersion = 26
-    const val targetSdkVersion = 29
+    const val targetSdkVersion = 32
     const val versionCode = 10001
     const val versionName = "1.0.1"
 }
